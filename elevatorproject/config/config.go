04c5932dd67a99686e1688c316bf66fe5b499eb6
Anja Burtonwood <anjavburtonwood@msn.com>
2023-03-20 14:06:43 +0100

    rett før vi legger til hra greier

--- elevatorproject/config/config.go
+++ elevatorproject/config/config.go
@@ -7,6 +7,6 @@ const NumFloors = 4
 // const LocalID = "Elevator1"
 // const SecondElev = "Elevator2"
 
-const LocalIP = "10.100.23.14:15657"
+const LocalIP = "10.100.23.32:15657"
 const LocalID = "Elevator2"
 const SecondElev = "Elevator1"

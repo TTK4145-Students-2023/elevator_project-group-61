732c39e51aa1f396bbf2c5a7db3d3bfd2af4b645
Nicolas <nicolarb@ntnu.no>
2023-03-24 15:35:57 +0100

    comment out new network code

--- elevatorproject/network/bcast/bcast.go
+++ elevatorproject/network/bcast/bcast.go
@@ -12,6 +12,7 @@ const bufSize = 1024
 
 // Encodes received values from `chans` into type-tagged JSON, then broadcasts
 // it on `port`
+
 func Transmitter(port int, chans ...interface{}) {
 	checkArgs(chans...)
 	typeNames := make([]string, len(chans))
@@ -43,6 +44,49 @@ func Transmitter(port int, chans ...interface{}) {
 
 	}
 }
+
+/*
+func Transmitter(port int, chans ...interface{}) {
+	checkArgs(chans...)
+	typeNames := make([]string, len(chans))
+	selectCases := make([]reflect.SelectCase, len(typeNames))
+	for i, ch := range chans {
+		selectCases[i] = reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: reflect.ValueOf(ch),
+		}
+		typeNames[i] = reflect.TypeOf(ch).Elem().String()
+	}
+
+	conn := conn.DialUDP("udp4", nil, &net.UDPAddr{
+		IP:   net.IPv4zero,
+		Port: port,
+	})
+	broadcastAddr, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("255.255.255.255:%d", port))
+	localAddr, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("127.0.0.1:%d", port))
+
+	for {
+		chosen, value, _ := reflect.Select(selectCases)
+		jsonstr, _ := json.Marshal(value.Interface())
+		ttj, _ := json.Marshal(typeTaggedJSON{
+			TypeId: typeNames[chosen],
+			JSON:   jsonstr,
+		})
+		if len(ttj) > bufSize {
+			panic(fmt.Sprintf(
+				"Tried to send a message longer than the buffer size (length: %d, buffer size: %d)\n\t'%s'\n"+
+					"Either send smaller packets, or go to network/bcast/bcast.go and increase the buffer size",
+				len(ttj), bufSize, string(ttj)))
+		}
+
+		// Send message to broadcast address
+		conn.WriteTo(ttj, broadcastAddr)
+
+		// Send message to localhost
+		conn.WriteTo(ttj, localAddr)
+	}
+}
+*/
 
 // Matches type-tagged JSON received on `port` to element types of `chans`, then
 // sends the decoded value on the corresponding channel
@@ -76,6 +120,45 @@ func Receiver(port int, chans ...interface{}) {
 		}})
 	}
 }
+
+/*
+func Receiver(port int, id string, chans ...interface{}) {
+	checkArgs(chans...)
+	chansMap := make(map[string]interface{})
+	for _, ch := range chans {
+		chansMap[reflect.TypeOf(ch).Elem().String()] = ch
+	}
+
+	var buf [bufSize]byte
+	conn := conn.DialBroadcastUDP(port)
+	for {
+		n, addr, e := conn.ReadFrom(buf[0:])
+		if e != nil {
+			fmt.Printf("bcast.Receiver(%d, ...):ReadFrom() failed: \"%+v\"\n", port, e)
+		}
+
+		var ttj typeTaggedJSON
+		json.Unmarshal(buf[0:n], &ttj)
+
+		// Check if the packet is sent from the same machine
+		if ttj.SenderID == id && addr.String() != "127.0.0.1" {
+			continue
+		}
+
+		ch, ok := chansMap[ttj.TypeId]
+		if !ok {
+			continue
+		}
+		v := reflect.New(reflect.TypeOf(ch).Elem())
+		json.Unmarshal(ttj.JSON, v.Interface())
+		reflect.Select([]reflect.SelectCase{{
+			Dir:  reflect.SelectSend,
+			Chan: reflect.ValueOf(ch),
+			Send: reflect.Indirect(v),
+		}})
+	}
+}
+*/
 
 type typeTaggedJSON struct {
 	TypeId string

1973d7a327d74c9cc58c0f4030266c8abc076082
jodderns lokale pc brukernavn :) <kristtjo@stud.ntnu.no>
2023-04-14 16:06:26 +0200

    changed some names

--- elevatorproject/singleelevator/single_elevator.go
+++ elevatorproject/singleelevator/single_elevator.go
@@ -8,22 +8,22 @@ import (
 
 // Har bare lagt til alt som trengs for spam
 
-func RunSingleElevator(ch_hra chan [config.NumFloors][2]bool, ch_cab_requests chan [config.NumFloors]bool, ch_completed_requests chan elevio.ButtonEvent, ch_new_requests chan elevio.ButtonEvent, ch_elevstate chan ElevState, ch_singleElevMode chan bool) {
+func RunSingleElevator(ch_hallRequests chan [config.NumFloors][2]bool, ch_cabRequests chan [config.NumFloors]bool, ch_completedRequest chan elevio.ButtonEvent, ch_newRequest chan elevio.ButtonEvent, ch_elevState chan ElevState, ch_singleElevMode chan bool) {
     // Channels    
-    drv_buttons := make(chan elevio.ButtonEvent)
-    drv_floors  := make(chan int)   
+    ch_buttons := make(chan elevio.ButtonEvent)
+    ch_floors  := make(chan int)   
 	ch_door := make(chan int)
     ch_error := make(chan int)
     
     // Elevio
-    go elevio.PollButtons(drv_buttons)
-    go elevio.PollFloorSensor(drv_floors)
+    go elevio.PollButtons(ch_buttons)
+    go elevio.PollFloorSensor(ch_floors)
 
     // Elevator timers
     go elevator_timers.CheckDoorTimer(ch_door)
     go elevator_timers.CheckErrorTimer(ch_error)
 
-    // single elev mode ch lagt til
-    Fsm_elevator(drv_buttons, drv_floors, ch_door, ch_error, ch_hra, ch_cab_requests, ch_completed_requests, ch_new_requests, ch_elevstate, ch_singleElevMode)
+    // Finite state machine
+    Fsm_elevator(ch_buttons, ch_floors, ch_door, ch_error, ch_hallRequests, ch_cabRequests, ch_completedRequest, ch_newRequest, ch_elevState, ch_singleElevMode)
 }
 
